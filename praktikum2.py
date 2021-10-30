bb297faf2122e31dde3fc04fd7eed33dde0845b3
Cecilia Sitompul <ceciliasitompul4@gmail.com>
2021-10-30 21:06:47 +0700

    Menambahkan gambar dan file program

---
+++ praktikum2.py
@@ -0,0 +1,7 @@
+phi = 3.14
+r = int(input("Masukkan jari-jari lingkaran : "))
+L = phi * (r * r)
+K = 2 * phi * r
+
+print("Keliling lingkaran dengan jari-jari {} adalah {}".format(r,K))
+print("Luas lingkaran dengan jari-jari {} adalah {}".format(r,L))
